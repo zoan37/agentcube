bb583a3e9a9d54ea2e2d6b597b5763ce419435e5
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-27 16:02:24 -0400

    demo

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+// @ts-ignore
+import { startDemo } from "./demo.js";
+
+// @ts-ignore
+// import { getWindowAI } from 'window.ai';
+
+onMounted(async () => {
+  startDemo();
+
+  // @ts-ignore
+  // const ai = await getWindowAI();
+});
+</script>
+
+<template>
+  <div id="divInfo">
+    drag and drop <a href="https://www.mixamo.com/" target="_blank" rel="noopener noreferrer">mixamo</a> animation
+    (.fbx)<br />
+  </div>
+</template>
+
+<style scoped>
+#divInfo {
+  padding: 8px;
+  position: fixed;
+  background: white;
+}
+</style>
